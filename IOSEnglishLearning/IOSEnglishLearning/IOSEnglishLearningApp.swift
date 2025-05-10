046eec9b93ab1c984215f2dd1d95a622d3502281
杨启仁 <wrnac370@163.com>
2025-05-10 14:32:52 +0800

    Create Xcode project

---
+++ IOSEnglishLearning/IOSEnglishLearning/IOSEnglishLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSEnglishLearningApp.swift
+//  IOSEnglishLearning
+//
+//  Created by yqr on 2025/5/10.
+//
+
+import SwiftUI
+
+@main
+struct IOSEnglishLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOSEnglishLearning/IOSEnglishLearningTests/IOSEnglishLearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOSEnglishLearningTests.swift
+//  IOSEnglishLearningTests
+//
+//  Created by yqr on 2025/5/10.
+//
+
+import Testing
+@testable import IOSEnglishLearning
+
+struct IOSEnglishLearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
